90222094824a5d24416ebfffce073aebc90d8c98
Rubenval <rnmjamsoft@gmail.com>
2021-05-26 22:30:12 -0300

    Security Config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
             <DonutChart/>
           </div>
         </div>
-        <div className="py-3">F
+        <div className="py-3">
           <h2 className="text-primary">Todas as Vendas</h2>
         </div>
         <DataTable />
